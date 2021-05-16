f15b0f580ddc75c4fb366ab27d3ae0b006e753dc
arturdavletshin <sadbooys.2001gmail.com>
2021-05-16 18:51:23 +0500

    fixing user didn't add to db from form

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.views import LoginView
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
@@ -6,13 +7,14 @@ from django.views.generic import FormView
 from django.contrib import messages
 from django.contrib.auth import authenticate
 
-from accounts.forms import NewUserForm
+from accounts.forms import NewUserForm, UserLoginForm
+from accounts.models import MyCustomUser
 
 
 class CustomLoginView(LoginView):
     template_name = 'accounts/login.html'
     success_url = reverse_lazy('home')
-
+    form_class =UserLoginForm
     def post(self, request, *args, **kwargs):
         print(request.POST)
         usrname = request.POST['username']
@@ -28,6 +30,17 @@ class RegistrationView(FormView):
     template_name = "accounts/register.html"
     success_url = "/login"
     form_class = NewUserForm
+
+    def post(self, request, *args, **kwargs):
+        data=request.POST
+        email=data['email']
+        password=data['password']
+        first_name=data['first_name']
+        second_name=data['second_name']
+        MyCustomUser.objects.create_user(email,password,first_name,second_name)
+        messages.info(request,f'Добро пожаловать: {first_name} {second_name}')
+        return redirect('/login')
+
 
 
 def home_page(request):
